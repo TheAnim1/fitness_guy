4f6a9f1a6d483c27999de3707d9be3d39e9b3932
unknown <animfahad32.com>
2022-10-11 17:58:35 +0600

    rapid api key updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,9 @@
 export const excerciseOption ={
 
         method: 'GET',
-        url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+       
         headers: {
-            'X-RapidAPI-Key': 'ac886ecc9cmsha0ad7cf3814ac9dp1dbe18jsn784548e1530c',
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
             'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
         }
     }
